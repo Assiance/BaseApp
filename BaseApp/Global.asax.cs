65eaae07b738c78dc22f48720ba1fc7514757cd6
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2015-07-21 02:50:20 -0400

    Added Dependency Injection

--- BaseApp/Global.asax.cs
+++ BaseApp/Global.asax.cs
@@ -6,11 +6,26 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using BaseApp.Infrastructure;
+using StructureMap;
+using StructureMap.Graph;
 
 namespace BaseApp
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        public IContainer Container
+        {
+            get
+            {
+                return (IContainer)HttpContext.Current.Items["_Container"];
+            }
+            set
+            {
+                HttpContext.Current.Items["_Container"] = value;
+            }
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -18,6 +33,29 @@ namespace BaseApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DependencyResolver.SetResolver(new StructureMapDependencyResolver(() => Container ?? ObjectFactory.Container));
+
+            ObjectFactory.Configure(cfg =>
+            {
+                cfg.Scan(scan =>
+                {
+                    scan.TheCallingAssembly();
+                    scan.WithDefaultConventions();
+                    scan.With(new ControllerConvention());
+                });
+            });
+        }
+
+        public void Application_BeginRequest()
+        {
+            Container = ObjectFactory.Container.GetNestedContainer();
+        }
+
+        public void Application_EndRequest()
+        {
+            Container.Dispose();
+            Container = null;
         }
     }
 }
